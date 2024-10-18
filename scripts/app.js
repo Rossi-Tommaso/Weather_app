ee05cbbf2b1d9347af592c47cb915f8b50921546
Romano-Gabriele <gabriele.romano@istitutoagnelli.it>
2024-10-18 18:35:23 +0200

    - fetch api

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,23 @@
-fetch('')
+const LATITUDE =  45.0705;
+const LONGITUDE = 7.6868;
+
+const LINK = `https://api.open-meteo.com/v1/forecast?latitude=${LATITUDE}&longitude=${LONGITUDE}&current=temperature_2m,relative_humidity_2m,precipitation,weather_code&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin`;
+
+fetch(LINK)
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Errore nella risposta della richiesta');
+        }
+        return response.json(); // Convertire in formato JSON
+    })
+    .then(data => {
+        console.log('Dati del meteo:', data);
+        updateTime(data);
+        // Puoi usare i dati qui per aggiornare l'interfaccia utente o altro
+    })
+
+function updateTime(data) {
+    const time = data.daily.time[0];
+    console.log("geouwiuwbgwariiyrihygrihgryggbryebygarigygrhrhagrihgarih");
+    console.log(time);
+}
