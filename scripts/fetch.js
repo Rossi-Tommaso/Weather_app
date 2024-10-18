da486f4e78d0ccfe18721037127ce3c512eb67f6
Romano-Gabriele <gabriele.romano@istitutoagnelli.it>
2024-10-19 00:12:32 +0200

    - aggiunta visualizzazione dei dati per ogni giorno della settimana

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -3,35 +3,38 @@ const LONGITUDE = 7.6868;
 
 const LINK = `https://api.open-meteo.com/v1/forecast?latitude=${LATITUDE}&longitude=${LONGITUDE}&current=temperature_2m,relative_humidity_2m,precipitation,weather_code&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin`;
 
-fetch(LINK)
-    .then(response => {
-        if (!response.ok) {
-        throw new Error('Errore nella risposta della richiesta');
-        }
-        return response.json(); // Convertire in formato JSON
-    })
-    .then(data => {
-        console.log('Dati del meteo:', data);
-        updateDay(data);
-        updateTime(data);
-        updateTemp(data);
-        // Puoi usare i dati qui per aggiornare l'interfaccia utente o altro
-    })
+function cetch() {
+    fetch(LINK)
+        .then(response => {
+            if (!response.ok) {
+            throw new Error('Errore nella risposta della richiesta');
+            }
+            return response.json(); // Convertire in formato JSON
+        })
+        .then(data => {
+            updateDay(data);
+            updateTime(data);
+            updateTemp(data);
+            // Puoi usare i dati qui per aggiornare l'interfaccia utente o altro
+        })
 
-function updateDay(data) {
-    const days = document.getElementsByClassName("day");
-    for(let i = 0; i < days.length; i++)
-        days[i].innerText = data.daily.time[i].slice(-2) + "/" + data.daily.time[i].slice(5, 7);
+    function updateDay(data) {
+        const days = document.getElementsByClassName("day");
+        for(let i = 0; i < days.length; i++)
+            days[i].innerText = data.daily.time[i].slice(-2) + "/" + data.daily.time[i].slice(5, 7);
+    }
+
+    function updateTime(data) {
+        const hours = document.getElementsByClassName("time");
+        for(let i = daynum; i < (hours.length + daynum); i++)
+            hours[i - daynum].innerText = data.hourly.time[i].slice(-5);
+    }
+
+    function updateTemp(data) {
+        const temps = document.getElementsByClassName("temp");
+        for(let i = daynum; i < (temps.length + daynum); i++)
+            temps[i - daynum].innerText = Math.round(data.hourly.temperature_2m[i]) + "°C";
+    }
 }
 
-function updateTime(data) {
-    const hours = document.getElementsByClassName("time");
-    for(let i = 0; i < hours.length; i++)
-        hours[i].innerText = data.hourly.time[i].slice(-5);
-}
-
-function updateTemp(data) {
-    const temps = document.getElementsByClassName("temp");
-    for(let i = 0; i < temps.length; i++)
-        temps[i].innerText = Math.round(data.hourly.temperature_2m[i]) + "°C";
-}
+cetch();
--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,12 @@
-const days = document.querySelectorAll('.day');
+const DAYLIST = [0, 24, 48, 72, 96, 120, 144];
+const DAYS = document.querySelectorAll('.day');
+let daynum = 0;
 
-days.forEach(day => {
+DAYS.forEach((day, index) => {
     day.addEventListener('click', () => {
         document.querySelector('.selected').classList.remove('selected');
         day.classList.add('selected');
+        daynum = DAYLIST[index];
+        cetch();
     });
 });
